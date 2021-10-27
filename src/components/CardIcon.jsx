8e23baae29533aa4cff37ab6b728da0490660984
itay haephrati <itay1313@gmail.com>
2021-10-27 18:34:59 +0300

    more design improvements toggle dark

--- src/components/CardIcon.jsx
+++ src/components/CardIcon.jsx
@@ -8,7 +8,7 @@ const CardIcon = ({ icon, title, desc, link }) => {
       data-sal-duration="2000"
       data-sal-easing="ease"
       to={link}
-      className="card border border-card-border shadow-lg hover:shadow-xl transition duration-300 ease-in-out compact side bg-card-background xl:w-5/12 w-full flex-grow p-7"
+      className="card border border-card-border shadow-lg hover:shadow-xl compact side bg-card-background xl:w-5/12 w-full flex-grow p-7"
     >
       <div className="flex-row items-center space-x-4 card-body">
         <div className="flex-1">
