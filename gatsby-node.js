301319ba4a40bbdaf24a481dd498a8909bfea20c
itay haephrati <itay1313@gmail.com>
2021-11-04 16:49:20 +0200

    gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,165 +1,177 @@
+const _ = require("lodash")
+const Promise = require("bluebird")
 const path = require("path")
+const slash = require("slash")
 
-exports.createPages = async ({ graphql, actions }) => {
+exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
-  const result = await graphql(`
-    {
-      allKontentItemNavigationItem(
-        filter: { system: { codename: { eq: "root" } } }
-      ) {
-        nodes {
-          elements {
-            subitems {
-              value {
-                ...folder
-                ...recursiveFolder
+  return new Promise((resolve, reject) => {
+    const navPagePath = path.resolve("./src/pages/nav-page.jsx")
+    const PageOverviewPath = path.resolve("./src/pages/page-overview.jsx")
+    const PageReleaseNotesPath = path.resolve(
+      "./src/pages/page-release-notes.jsx"
+    )
+    const PageGeneralPath = path.resolve("./src/pages/page-jekyll-markdown.jsx")
+
+    graphql(`
+      {
+        allKontentItemNavigationItem(
+          filter: { system: { codename: { eq: "root" } } }
+        ) {
+          nodes {
+            elements {
+              subitems {
+                value {
+                  ...folder
+                  ...recursiveFolder
+                }
+              }
+              url {
+                value
               }
             }
-            url {
-              value
+          }
+        }
+        allKontentItemKcProductOverview {
+          nodes {
+            system {
+              codename
+              id
             }
-          }
-        }
-      }
-      allKontentItemKcProductOverview {
-        nodes {
-          system {
-            codename
-            id
-          }
-          elements {
-            url_slug {
-              name
-              value
-            }
-          }
-        }
-      }
-      allKontentItemKnowledgeCenterMarkdownPage {
-        nodes {
-          system {
-            codename
-            id
-          }
-          elements {
-            permalink {
-              name
-              value
-            }
-          }
-        }
-      }
-      allKontentItemReleaseNotes {
-        nodes {
-          system {
-            codename
-            id
-          }
-          elements {
-            url_slug {
-              value
-              name
-            }
-          }
-        }
-      }
-    }
-
-    fragment folder on kontent_item_navigation_item {
-      system {
-        type
-        id
-      }
-      elements {
-        url {
-          value
-        }
-        title {
-          value
-        }
-      }
-    }
-
-    fragment page on kontent_item {
-      ...PO
-      ...RN
-      ...KCMD
-    }
-
-    fragment KCMD on kontent_item_knowledge_center_markdown_page {
-      elements {
-        pagename {
-          value
-        }
-        permalink {
-          value
-        }
-      }
-      system {
-        id
-        type
-      }
-    }
-
-    fragment RN on kontent_item_release_notes {
-      elements {
-        title {
-          value
-        }
-        url_slug {
-          value
-        }
-      }
-      system {
-        id
-        type
-      }
-    }
-
-    fragment PO on kontent_item_kc_product_overview {
-      elements {
-        title {
-          value
-        }
-        url_slug {
-          value
-        }
-      }
-      system {
-        id
-        type
-      }
-    }
-
-    fragment recursiveFolder on kontent_item_navigation_item {
-      system {
-        type
-        codename
-      }
-      elements {
-        subitems {
-          value {
-            ...page
-            ...folder
-            ... on kontent_item_navigation_item {
-              system {
-                type
+            elements {
+              url_slug {
+                name
+                value
               }
-              elements {
-                subitems {
-                  value {
-                    ...page
-                    ...folder
-                    ... on kontent_item_navigation_item {
-                      system {
-                        type
-                      }
-                      elements {
-                        subitems {
-                          value {
-                            ...page
-                            ...folder
+            }
+          }
+        }
+        allKontentItemKnowledgeCenterMarkdownPage {
+          nodes {
+            system {
+              codename
+              id
+            }
+            elements {
+              permalink {
+                name
+                value
+              }
+            }
+          }
+        }
+        allKontentItemReleaseNotes {
+          nodes {
+            system {
+              codename
+              id
+            }
+            elements {
+              url_slug {
+                value
+                name
+              }
+            }
+          }
+        }
+      }
+
+      fragment folder on kontent_item_navigation_item {
+        system {
+          type
+          id
+        }
+        elements {
+          url {
+            value
+          }
+          title {
+            value
+          }
+        }
+      }
+
+      fragment page on kontent_item {
+        ...PO
+        ...RN
+        ...KCMD
+      }
+
+      fragment KCMD on kontent_item_knowledge_center_markdown_page {
+        elements {
+          pagename {
+            value
+          }
+          permalink {
+            value
+          }
+        }
+        system {
+          id
+          type
+        }
+      }
+
+      fragment RN on kontent_item_release_notes {
+        elements {
+          title {
+            value
+          }
+          url_slug {
+            value
+          }
+        }
+        system {
+          id
+          type
+        }
+      }
+
+      fragment PO on kontent_item_kc_product_overview {
+        elements {
+          title {
+            value
+          }
+          url_slug {
+            value
+          }
+        }
+        system {
+          id
+          type
+        }
+      }
+
+      fragment recursiveFolder on kontent_item_navigation_item {
+        system {
+          type
+          codename
+        }
+        elements {
+          subitems {
+            value {
+              ...page
+              ...folder
+              ... on kontent_item_navigation_item {
+                system {
+                  type
+                }
+                elements {
+                  subitems {
+                    value {
+                      ...page
+                      ...folder
+                      ... on kontent_item_navigation_item {
+                        system {
+                          type
+                        }
+                        elements {
+                          subitems {
+                            value {
+                              ...page
+                              ...folder
+                            }
                           }
                         }
                       }
@@ -171,64 +183,89 @@ exports.createPages = async ({ graphql, actions }) => {
           }
         }
       }
-    }
-  `)
+    `).then(result => {
+      if (result.errors) {
+        console.error(result.errors)
+        reject(result.errors)
+      }
 
-  const pages =
-    result.data.allKontentItemNavigationItem.nodes[0].elements.subitems.value
+      // _.each(result.data.allKontentItemNavigationItem.nodes, node => {
+      //   const contentPage = node.elements.subitems.value[0]
+      //   contentPage && createPage({
+      //     path: `/${node.elements.url.value}/`,
+      //     component: slash(navPagePath),
+      //     context: {
+      //       //check if the page is a nav item
 
-  const walkTree = (item, url, crumbs) => {
-    if (item.system.type === "navigation_item") {
-      const newUrl =
-        url !== "" ? `${item.elements.url.value}` : item.elements.url.value
-      const folderCrumbs = crumbs
-      folderCrumbs.push(item.elements.title.value)
-      item.elements.subitems.value.forEach(item =>
-        walkTree(item, newUrl, folderCrumbs)
+      //       codename: `${contentPage.system.codename}`,
+      //     },
+      //   })
+      // })
+
+      _.each(result.data.allKontentItemNavigationItem.nodes, node => {
+        createPage({
+          path: `/${node.elements.url.value}/`,
+          component: slash(navPagePath),
+          context: { slug: `${node.elements.url.value}` },
+        })
+      })
+
+      _.each(result.data.allKontentItemKcProductOverview.nodes, node => {
+        createPage({
+          path: `/${node.elements.url_slug.value}/`,
+          component: slash(PageOverviewPath),
+          context: {
+            systemId: node.system.id,
+            slug: `${node.elements.url_slug.value}`
+          },
+        })
+      })
+
+      _.each(result.data.allKontentItemReleaseNotes.nodes, node => {
+        createPage({
+          path: `/${node.elements.url_slug.value}/`,
+          component: slash(PageReleaseNotesPath),
+          context: { slug: `${node.elements.url_slug.value}` },
+        })
+      })
+
+      _.each(
+        result.data.allKontentItemKnowledgeCenterMarkdownPage.nodes,
+        node => {
+          createPage({
+            path: `/${node.elements.permalink.value}/`,
+            component: slash(PageGeneralPath),
+            context: {
+              systemId: node.system.id,
+              slug: `${node.elements.permalink.value}`
+            },
+          })
+        }
       )
-      createPage({
-        // path: `/${url}-${item.elements.url_slug.value}`,
-        path: `/${item.elements.url.value}`,
-        component: path.resolve(`./src/pages/nav-page.js`),
-        context: {
-          systemId: item.system.id,
-        },
-      })
-    } else if (item.system.type === "kc_product_overview") {
-      const pageCrumbs = [...crumbs]
-      pageCrumbs.push(item.elements.title.value)
-      createPage({
-        path: `/${item.elements.url_slug.value}`,
-        component: path.resolve(`./src/pages/page-overview.js`),
-        context: {
-          breadCrumbs: pageCrumbs,
-          systemId: item.system.id,
-        },
-      })
-    } else if (item.system.type === "release_notes") {
-      createPage({
-        path: `/${item.elements.url_slug.value}`,
-        component: path.resolve(`./src/pages/page-release-notes.js`),
-        context: {
-          systemId: item.system.id,
-        },
-      })
-    } else if (item.system.type === "knowledge_center_markdown_page") {
-      const pageCrumbs = [...crumbs]
-      pageCrumbs.push(item.elements.pagename.value)
-      createPage({
-        path: `/${item.elements.permalink.value}`,
-        component: path.resolve(`./src/pages/page-jekyll-markdown.js`),
-        context: {
-          breadCrumbs: pageCrumbs,
-          systemId: item.system.id,
-        },
-      })
-    }
-  }
 
-  // Create pages
-  pages.forEach(node => {
-    walkTree(node, "", [])
+      // const tags = result.data.allKontentItemTag.nodes
+      // _.each(tags, tag => {
+      //   const tagCodename = tag.system.codename
+      //   const tagTitle = tag.elements.title.value
+      //   createPage({
+      //     path: `/tags/${tag.elements.slug.value}/`,
+      //     component: PageReleaseNotesPath,
+      //     context: { tagCodename, tagTitle },
+      //   })
+      // })
+
+      // const categories = result.data.allKontentItemCategory.nodes
+      // _.each(categories, category => {
+      //   const categoryCodename = category.system.codename
+      //   const categoryTitle = category.elements.title.value
+      //   createPage({
+      //     path: `/categories/${category.elements.slug.value}/`,
+      //     component: categoryTemplate,
+      //     context: { categoryCodename, categoryTitle },
+      //   })
+      // })
+
+      resolve()
+    })
   })
 }
