254af5dc8a50a109cde2660f71ebae25aa7184bf
itay haephrati <itay1313@gmail.com>
2021-12-08 11:59:22 +0200

    p space

--- .storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-actions", "@storybook/addon-links", "@storybook/addon-viewport/register", "@storybook/addon-storysource"],
-  "framework": "@storybook/react",
-  core: {
-    builder: "webpack5"
-  }
-};
