e025ce6a6e9375849b204093edc167ce34f764dc
itay haephrati <itay1313@gmail.com>
2021-12-11 19:59:28 +0200

    jumpto bug

--- src/pages/post-release-notes.jsx
+++ src/pages/post-release-notes.jsx
@@ -83,7 +83,7 @@ const ReleaseNotesPostTemplate = ({ data, pageContext }) => {
                       <div key={node?.system?.name}>{node?.system?.name}</div>
                     ))}
                   </h2>{" "}
-                  <h2 className="h4" style={{ margin: 0 }}>
+                  <h4 className="h4" style={{ margin: 0 }}>
                     <time
                       className="release-notes-item-time flex items-center"
                       dateTime={moment(
@@ -95,7 +95,7 @@ const ReleaseNotesPostTemplate = ({ data, pageContext }) => {
                         "MMMM YY"
                       )}
                     </time>
-                  </h2>
+                  </h4>
                 </div>
 
                 {/* Features */}
