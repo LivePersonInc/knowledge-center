6d6397c87385b912a4d64d894fc82ec4af7206f5
itay haephrati <itay1313@gmail.com>
2021-12-19 16:27:40 +0200

    trying to solve bug with wrappageelement

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,74 @@
 import "./src/assets/scss/index.scss"
+
+import React from "react"
+import styled from "styled-components"
+import Sidebar from "./src/components/Sidebar"
+import Footer from "./src/components/Footer"
+import Header from "./src/components/Header"
+import GlobalStyles from "./src/styles/GlobalStyles"
+
+
+const BodyStyles = styled.div`
+  min-height: 100vh;
+  display: grid;
+  align-content: space-between;
+  grid-template-rows: auto 1fr auto;
+  .anchorlist {
+    height: 100%;
+    justify-content: flex-start;
+  }
+  .drawer {
+    overflow: visible;
+  }
+  .drawer-toggle ~ .drawer-content {
+    overflow-y: visible;
+  }
+`
+
+const SiteLayoutStyles = styled.main`
+  width: 100%;
+  display: grid;
+  grid-template-areas: "content";
+  grid-template-columns: 1fr;
+  @media (max-width: 1100px) {
+    grid-template-columns: 1fr;
+  }
+`
+
+const ContentStyles = styled.div`
+  padding: 1.75rem;
+  @media (min-width: 1024px) {
+    padding-right: 0;
+  }
+  @media (max-width: 767px) {
+    padding: 1rem 0 0 0;
+    & > div {
+      padding: 0 25px;
+    }
+  }
+`
+
+export function wrapPageElement({ element, props }) {
+  const Layout = element.type.Layout ?? React.Fragment
+  return (
+    <Layout {...props}>
+      <BodyStyles
+        className="layout"
+      >
+
+        <GlobalStyles />
+        <Header />
+        <div className="max-width w-full min-h-screen drawer drawer-mobile overflow-y-visible">
+          <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+          <div className="flex flex-col items-center justify-start drawer-content">
+            <SiteLayoutStyles>
+              <ContentStyles>{element}</ContentStyles>
+            </SiteLayoutStyles>
+          </div>
+          <Sidebar />
+        </div>
+        <Footer />
+      </BodyStyles>
+    </Layout>
+  )
+}
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,109 +1,36 @@
-import React, { useEffect } from "react"
-import { StaticQuery, graphql } from "gatsby"
-import styled from "styled-components"
-import KontentSmartLink from "@kentico/kontent-smart-link"
-import Header from "./Header"
-import Footer from "./Footer"
-import GlobalStyles from "../styles/GlobalStyles"
-import Sidebar from "./Sidebar"
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
 import Seo from "./Seo"
 
-const BodyStyles = styled.div`
-  min-height: 100vh;
-  display: grid;
-  align-content: space-between;
-  grid-template-rows: auto 1fr auto;
-  .anchorlist {
-    height: 100%;
-    justify-content: flex-start;
-  }
-  .drawer {
-    overflow: visible;
-  }
-  .drawer-toggle ~ .drawer-content {
-    overflow-y: visible;
-  }
-`
-
-const SiteLayoutStyles = styled.main`
-  width: 100%;
-  display: grid;
-  grid-template-areas: "content";
-  grid-template-columns: 1fr;
-  @media (max-width: 1100px) {
-    grid-template-columns: 1fr;
-  }
-`
-
-const ContentStyles = styled.div`
-  padding: 1.75rem;
-  @media (min-width: 1024px) {
-    padding-right: 0;
-  }
-  @media (max-width: 767px) {
-    padding: 1rem 0 0 0;
-    & > div {
-      padding: 0 25px;
-    }
-  }
-`
-
 const Layout = ({ children }) => {
-  useEffect(() => {
-    const plugin = KontentSmartLink.initialize({
-      queryParam: "preview-mode",
-    })
-    return () => {
-      plugin.destroy()
+  const data = useStaticQuery(graphql`
+    {
+      sitePlugin(name: { eq: "@kentico/gatsby-source-kontent" }) {
+        pluginOptions {
+          projectId
+          languageCodenames
+        }
+      }
+      site {
+        siteMetadata {
+          title
+        }
+      }
     }
-  })
-
-  // useEffect(() => {
-  //   console.log("render layout")
-  // }, [])
+  `)
 
   return (
-    <StaticQuery
-      query={graphql`
-        {
-          sitePlugin(name: { eq: "@kentico/gatsby-source-kontent" }) {
-            pluginOptions {
-              projectId
-              languageCodenames
-            }
-          }
-          site {
-            siteMetadata {
-              title
-            }
-          }
-        }
-      `}
-      render={data => (
-        <BodyStyles
-          className="layout"
-          data-kontent-project-id={data.sitePlugin.pluginOptions.projectId}
-          data-kontent-language-codename={
-            data.sitePlugin.pluginOptions.languageCodenames[0]
-          }
-        >
-          <Seo title={data.site.siteMetadata.title} />
-          <GlobalStyles />
-          <Header />
-          <div className="max-width w-full min-h-screen drawer drawer-mobile overflow-y-visible">
-            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="flex flex-col items-center justify-start drawer-content">
-              <SiteLayoutStyles>
-                <ContentStyles>{children}</ContentStyles>
-              </SiteLayoutStyles>
-            </div>
-            <Sidebar />
-          </div>
-          <Footer />
-        </BodyStyles>
-      )}
-    ></StaticQuery>
+    <>
+      <Seo title={data.site.siteMetadata.title} />
+
+      {children}
+    </>
   )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
 }
 
 export default Layout
