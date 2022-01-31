4beba8e139f257f91425204519c27ed0704127b4
itay haephrati <itay1313@gmail.com>
2022-02-01 00:14:32 +0200

    toggle  text

--- src/components/ThemeToggler.jsx
+++ src/components/ThemeToggler.jsx
@@ -15,9 +15,10 @@ export default function ThemeToggle() {
                 onChange={e => toggleTheme(e.target.checked ? "dark" : "light")}
                 checked={theme === "dark"}
               />
-              <div className="slider round"></div>
-              <span className="toggle-title-light">Light theme</span>
-              <span className="toggle-title-dark">Dark theme</span>
+              <div className="slider round">
+                <span className="toggle-title-light">Light theme</span>
+                <span className="toggle-title-dark">Dark theme</span>
+              </div>
             </label>
           </div>
         )
