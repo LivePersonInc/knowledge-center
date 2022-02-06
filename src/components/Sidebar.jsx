f8ccab67a0690b597e046bf88fd49ee19af3945b
itay haephrati <itay1313@gmail.com>
2022-02-06 23:17:07 +0200

    withuin

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,7 +14,6 @@ const SidebarStyles = styled.div`
   }
   button,
   a {
-    &:focus,
     &:focus-within {
       color: var(--link-color-hover);
       border: 2px dashed var(--link-color-hover);
