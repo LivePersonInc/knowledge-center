1e56ea87cdc079628b6293796c5b9b0b9cf6ac1d
itay haephrati <itay1313@gmail.com>
2022-02-15 00:13:32 +0200

    fonts

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -21,8 +21,8 @@ export default function Seo({ children, location, description, title, image }) {
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       <link rel="alternate icon" href="/favicon.ico" />
       {/* Meta Tags */}
+      <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta charSet="utf-8" />
       <meta name="description" content={site.siteMetadata.description} />
       {/* Open Graph */}
       {location && <meta property="og:url" content={location.href} />}
