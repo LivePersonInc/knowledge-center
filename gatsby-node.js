97ef8b7ff20bc6ae5046ff040834d61582e20ec4
itay haephrati <itay1313@gmail.com>
2022-09-08 10:46:51 +0300

    test

--- gatsby-node.js
+++ gatsby-node.js
@@ -900,3 +900,17 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 }
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html" || stage === "develop-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /bad-module/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
